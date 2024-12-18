796c42abb858b5b594c2fb9a3494b63877cf9ba6
ikerbenitez <ikerbenitezsoria@gmail.com>
2024-12-18 15:32:54 +0100

    vite multipage

--- regalo/vite.config.js
+++ regalo/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+// import multiPages from 'vite-plugin-multi-pages';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html', // Página principal
+        certificado: 'certificado.html', // Página adicional
+      },
+    },
+  },
 })
